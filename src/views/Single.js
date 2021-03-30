e73840dac0aba1038a666a62a8d6b94f5d55aee3
Aili Priimann <ailipriimann@Ailis-MacBook-Pro.local>
2021-03-30 16:03:30 +0300

    single

--- src/views/Single.js
+++ src/views/Single.js
@@ -1,13 +1,42 @@
 import {uploadsUrl} from '../utils/variables';
 import PropTypes from 'prop-types';
+// eslint-disable-next-line max-len
+import {Card, Typography, CardActionArea, CardMedia, CardContent} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: '100%',
+  },
+  media: {
+    height: '50vh',
+  },
+});
 
 const Single = ({location}) => {
+  const classes = useStyles();
   const file = location.state;
 
   return (
     <>
-      <h1>{file.title}</h1>
-      <img src={uploadsUrl + file.filename} alt={file.title}/>
+      <Typography component="h1" variant="h2">{file.title}</Typography>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={uploadsUrl + file.filename}
+            title={file.title}
+          />
+          <CardContent>
+            <Typography>
+              {file.description}
+            </Typography>
+            <Typography variant="subtitle2">
+              {file.user_id}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </>
   );
 };
