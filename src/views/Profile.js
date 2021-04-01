f3a262199cc66029c8675a0a5858ae8b186728fe
Aili Priimann <ailipriimann@Ailis-MacBook-Pro.local>
2021-04-01 13:02:25 +0300

    avatar

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,7 +1,18 @@
 import {useContext} from 'react';
 import {MediaContext} from '../contexts/MediaContext';
+import {makeStyles} from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 const Profile = () => {
+  const classes = useStyles();
   // const user = JSON.parse(localStorage.getItem('user'));
   const [user] = useContext(MediaContext);
 
@@ -9,7 +20,8 @@ const Profile = () => {
     <>
       <h1>Profile</h1>
       {user &&
-      <div>
+      <div className={classes.root}>
+        <Avatar alt="avatar picture" src="/static/images/avatar/1.jpg" />
         <p>{user.full_name}</p>
         <p>{user.email}</p>
         <p>{user.username}</p>
